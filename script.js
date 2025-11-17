7e90119d65773da5ee7c9e736a9721c54fe40752
Kaneki3332 <gabi.mihnea46@gmail.com>
2025-11-17 20:27:35 +0200

    versiune aproape finala

--- script.js
+++ script.js
@@ -1,28 +1,115 @@
-
-const imagini = [
-  "Resources/Seturi de mot/set_mot1.jpg",
-  "Resources/Seturi de mot/set_mot2.jpg",
-  "Resources/Seturi de mot/set_mot3.jpg",
-  "Resources/Seturi de mot/set_mot0.jpg",
+// === GALERIE CU LIGHTBOX ===
+const imaginiMot = [
+  "resources/Seturi de mot/set_mot0.jpg",
+  "resources/Seturi de mot/set_mot1.jpg",
+  "resources/Seturi de mot/set_mot2.jpg",
+  "resources/Seturi de mot/set_mot3.jpg"
 ];
 
-let index = 0;
-const slide = document.getElementById("slide");
+const imaginiMarturii = [
+  "resources/Marturii botez/marturii_botez0.jpg",
+  "resources/Marturii botez/marturii_botez1.jpg",
+  "resources/Marturii botez/marturii_botez2.jpg",
+  "resources/Marturii botez/marturii_botez3.jpg"
+];
 
-// Funcții pentru schimbarea imaginilor
-function schimbaImagineNext() {
-  index = (index + 1) % imagini.length;
-  slide.src = imagini[index];
+const imaginiPahare = [
+  "Resources/Pahare personalizate/pahare0.jpg",
+  "Resources/Pahare personalizate/pahare1.jpg",
+  "Resources/Pahare personalizate/pahare2.jpg",
+  "Resources/Pahare personalizate/pahare3.jpg"
+];
+
+const imaginiTricouri = [
+  "Resources/Tricouri personalizate/tricouri0.jpg",
+  "Resources/Tricouri personalizate/tricouri1.jpg",
+  "Resources/Tricouri personalizate/tricouri2.jpg",
+  "Resources/Tricouri personalizate/tricouri3.jpg"
+];
+
+const imaginiTrusouri = [
+  "Resources/Trusouri botez/trusou_botez0.jpg",
+  "Resources/Trusouri botez/trusou_botez1.jpg",
+  "Resources/Trusouri botez/trusou_botez2.jpg",
+  "Resources/Trusouri botez/trusou_botez3.jpg"
+];
+
+const imaginiSeturiCadou = [
+  "Resources/Seturi cadou/seturi_cadou0.jpg",
+  "Resources/Seturi cadou/seturi_cadou1.jpg",
+  "Resources/Seturi cadou/seturi_cadou2.jpg",
+  "Resources/Seturi cadou/seturi_cadou3.jpg"
+];
+
+const imaginiInvitatii = [
+  "Resources/Invitatii/invitatii0.jpg",
+  "Resources/Invitatii/invitatii1.jpg",
+  "Resources/Invitatii/invitatii2.jpg",
+  "Resources/Invitatii/invitatii3.jpg"
+];
+
+// === LIGHTBOX ===
+const mainImg = document.getElementById("main-img");
+const lightbox = document.getElementById("lightbox");
+const lightboxImg = document.getElementById("lightbox-img");
+
+if (mainImg && lightbox && lightboxImg) {
+  let currentGallery = [];
+  const galleryType = mainImg.dataset.gallery;
+
+  if (galleryType === "mot") currentGallery = imaginiMot;
+  else if (galleryType === "marturii") currentGallery = imaginiMarturii;
+  else if (galleryType === "pahare") currentGallery = imaginiPahare;
+  else if (galleryType === "tricouri") currentGallery = imaginiTricouri;
+  else if (galleryType === "trusouri") currentGallery = imaginiTrusouri;
+  else if (galleryType === "seturi_cadou") currentGallery = imaginiSeturiCadou;
+  else if (galleryType === "invitatii") currentGallery = imaginiInvitatii;
+
+  let index = 0;
+
+  mainImg.addEventListener("click", () => {
+    index = 0;
+    lightboxImg.src = currentGallery[index];
+    lightbox.style.display = "flex";
+  });
+
+  document.querySelector(".close")?.addEventListener("click", () => {
+    lightbox.style.display = "none";
+  });
+
+  document.querySelector(".lightbox-next")?.addEventListener("click", () => {
+    index = (index + 1) % currentGallery.length;
+    lightboxImg.src = currentGallery[index];
+  });
+
+  document.querySelector(".lightbox-prev")?.addEventListener("click", () => {
+    index = (index - 1 + currentGallery.length) % currentGallery.length;
+    lightboxImg.src = currentGallery[index];
+  });
+
+  lightbox.addEventListener("click", (e) => {
+    if (e.target === lightbox) {
+      lightbox.style.display = "none";
+    }
+  });
 }
 
-function schimbaImaginePrev() {
-  index = (index - 1 + imagini.length) % imagini.length;
-  slide.src = imagini[index];
+// === EMAILJS ===
+(function() {
+  emailjs.init("l2Zi2yl3gvIzRGOYl"); // cheia ta publică
+})();
+
+const formular = document.getElementById("formular-contact");
+if (formular) {
+  formular.addEventListener("submit", function(e) {
+    e.preventDefault();
+
+    emailjs.sendForm("service_eyvaylg", "template_ptcredn", this)
+      .then(function() {
+        alert("Mesajul a fost trimis cu succes! 🎉");
+      }, function(error) {
+        alert("A apărut o eroare. Încearcă din nou.");
+        console.error("EmailJS error:", error);
+      });
+  });
 }
-
-// Evenimente pentru butoane
-document.querySelector(".next").addEventListener("click", schimbaImagineNext);
-document.querySelector(".prev").addEventListener("click", schimbaImaginePrev);
-
-// Schimbare automată la fiecare 4 secunde
-setInterval(schimbaImagineNext, 4000);
