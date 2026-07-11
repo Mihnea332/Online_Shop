5b142beac292417dc806617f53ec044fa6ff5dcd
Kaneki3332 <gabi.mihnea46@gmail.com>
2026-07-11 17:17:14 +0300

    ff

--- backend/src/app.js
+++ backend/src/app.js
@@ -11,7 +11,10 @@ const app = express();
 app.use(cookieParser());
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: [
+      "http://localhost:5173",
+      "https://online-shop-44y1-nine.vercel.app",
+    ],
     credentials: true,
   }),
 );
